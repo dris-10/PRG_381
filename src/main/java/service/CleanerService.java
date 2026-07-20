a441391aafc1cd3f6e9d248446b3faff9c358ae5
Blkswrn <603241@student.belgiumcampus.ac.za>
2026-07-20 09:14:49 +0200

    Added the functions for the model, service, DAO and controller files on the cleaner crud file. The code works with the following needing to be worked on before moving the code to the main branch: 1. database connection 2. dashboards

--- src/main/java/service/CleanerService.java
+++ src/main/java/service/CleanerService.java
@@ -1,4 +1,51 @@
 package service;
 
+import dao.CleanerDAO;
+import model.Cleaner;
+import util.Validation;
+
+import java.util.List;
+
 public class CleanerService {
+    //show dependency
+    private CleanerDAO cleanerDAO;
+    public CleanerService(){
+        cleanerDAO = new CleanerDAO();
+    }
+    //adding a cleaner
+    public boolean addCleaner(Cleaner cleaner){
+        if(!Validation.validCleaner(cleaner)){
+            return false;
+        }
+        return cleanerDAO.addCleaner(cleaner);
+    }
+    //get all cleaners
+    public List<Cleaner> getAllCleaners(){
+       return cleanerDAO.getAllCleaners();
+    }
+    //find cleaner by using their id
+    public Cleaner findCleaner(int cleanerId){
+        if(!Validation.validFindId(cleanerId)){
+            return null;
+        }
+        return cleanerDAO.findCleaner(cleanerId);
+    }
+    //update cleaner
+    public boolean updateCleaner(Cleaner cleaner){
+        if(!Validation.validCleaner(cleaner)){
+            return false;
+        }
+        if(!Validation.validId(cleaner.getCleanerId())){
+            return false;
+        }
+        return cleanerDAO.updateCleaner(cleaner);
+    }
+    //delete cleaner
+    public boolean deleteCleaner(int cleanerId){
+        if(!Validation.validDeleteId(cleanerId)) {
+            return false;
+        }
+        return cleanerDAO.deleteCleaner(cleanerId);
+    }
+
 }
